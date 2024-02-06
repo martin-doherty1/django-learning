58d9992099894f6ed51be263576bba8d539a9b37
martin.doherty@kainos.com <martin.doherty@kainos.com>
2024-02-06 13:31:48 +0000

    added tests and updated gitignore

--- blog/serializers.py
+++ blog/serializers.py
@@ -13,7 +13,17 @@ class UserSerializer(serializers.ModelSerializer):
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = "__all__"
+        fields = ['name', 'description']
+
+    def validate_name(self, value):
+        if not (value and value.strip()):
+            raise serializers.ValidationError("Name must be not be blank or null")
+        return value
+
+    def validate_description(self, value):
+        if not (value and value.strip()):
+            raise serializers.ValidationError("Description must be not be blank or null")
+        return value
 
 
 class ArticleSerializer(serializers.ModelSerializer):
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from rest_framework.reverse import reverse
+
+from blog.models import Category
+
 
 # Create your tests here.
+def create_category(category_name, category_description):
+    return Category.objects.create(name=category_name, description=category_description)
+
+class ArticleTests(TestCase):
+    def test_no_Articles(self):
+        """
+        If no questions exist, an appropriate message is displayed.
+        """
+        print(reverse("article-list"))
+        response = self.client.get(reverse("article-list"))
+        self.assertEqual(response.status_code, 200)
+
+
+class CategoryTests(TestCase):
+
+    def test_with_one_category(self):
+        category = create_category(category_name="Winner", category_description="pool")
+        data = {"pk": category.id}
+        response = self.client.get(reverse("category-detail", kwargs=data))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["name"], category.name)
+        self.assertEqual(response.data["description"], category.description)
+
+    def test_category_post(self):
+        data = {'name': "testPost", 'description': "testPost"}
+        response = self.client.post(reverse("category-list"), data)
+        self.assertEqual(Category.objects.last().name, data["name"])
+        self.assertEqual(response.status_code, 201)
+
